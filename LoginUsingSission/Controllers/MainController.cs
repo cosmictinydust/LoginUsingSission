368ee4b1d1ffe02577f1623405460de489972e6f
随意 <cosmictinydust@hotmail.com>
2020-02-06 01:50:01 +0800

    添加项目文件。

---
+++ LoginUsingSission/Controllers/MainController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LoginUsingSission.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoginUsingSission.Controllers
+{
+    public class MainController : Controller
+    {
+        umsContext ORM = null;
+        public MainController(umsContext _ORM)
+        {
+            ORM = _ORM;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult RegisterNewUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult RegisterNewUser(SystemUsers u)
+        {
+            try
+            {
+                u.Role = "Staff";
+                u.Status = "Active";
+                ORM.SystemUsers.Add(u);
+                ORM.SaveChanges();
+                ViewBag.Message = "用户 " + u.UserName + " 注册成功！";
+            }
+            catch(Exception e)
+            {
+                ViewBag.Message = "新用户注册失败! "+e.Message.ToString();
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            ViewBag.LTLD = Request.Cookies["LastLoggedInTime"];
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(SystemUsers u)
+        {
+            var LoggedInUser = ORM.SystemUsers.Where(x => x.UserName == u.UserName && x.Password == u.Password).FirstOrDefault();
+
+            if (LoggedInUser == null)
+            {
+                ViewBag.Message = "用户名或密码错误";
+                return View();
+            }
+
+            HttpContext.Session.SetString("UserName", LoggedInUser.UserName);
+            HttpContext.Session.SetString("Role", LoggedInUser.Role);
+
+            Response.Cookies.Append("LastLoggedInTime", DateTime.Now.ToString());
+
+            return RedirectToAction("Dashboard");
+        }
+
+        public IActionResult Dashboard() 
+        {
+            if (string.IsNullOrEmpty(HttpContext.Session.GetString("UserName")))
+            {
+                return RedirectToAction("Login");
+            }
+            ViewBag.UserName = HttpContext.Session.GetString("UserName");
+            ViewBag.Role = HttpContext.Session.GetString("Role");
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ LoginUsingSission/Models/umsContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace LoginUsingSission.Models
+{
+    public partial class umsContext : DbContext
+    {
+        public umsContext()
+        {
+        }
+
+        public umsContext(DbContextOptions<umsContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<SystemUsers> SystemUsers { get; set; }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SystemUsers>(entity =>
+            {
+                entity.Property(e => e.Email).HasMaxLength(50);
+
+                entity.Property(e => e.Password).HasMaxLength(50);
+
+                entity.Property(e => e.Role).HasMaxLength(50);
+
+                entity.Property(e => e.Status).HasMaxLength(50);
+
+                entity.Property(e => e.UserName).HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
